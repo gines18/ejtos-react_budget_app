805318f52f92bcdc7c7c8a04223b28719ec66559
gines18 <ruczkowski.m@gmail.com>
2023-08-26 14:40:55 +0100

    add functionality to budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,14 +1,51 @@
-
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
-
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import "./Budget.css";
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+  const { budget, expenses } = useContext(AppContext);
+  const [editableBudget, setEditableBudget] = useState(budget);
+
+  const handleBudgetChange = (event) => {
+    setEditableBudget(event.target.value);
+  };
+
+  const budgetChangeIncrease = () => {
+    const newBudget = parseInt(editableBudget) + 10;
+    setEditableBudget(newBudget);
+  };
+
+  const budgetChangeDecrease = () => {
+    const newBudget = parseInt(editableBudget) - 10;
+    setEditableBudget(newBudget);
+    if (newBudget < expenses.reduce((total, item) => total + item.cost, 0)) {
+      alert("You cannot reduce the budget value lower than ehe spending!");
+    }
+  };
+
+  return (
+    <div id="container-budget" className="alert alert-secondary">
+      Budget: £
+      <input
+        type="text"
+        name="myInput"
+        value={editableBudget}
+        onChange={handleBudgetChange}
+      />
+      <div>
+        <div>
+          <button onClick={budgetChangeIncrease}>
+            {" "}
+            <img src="./up.png" style={{ width: "7px" }} alt="Up Arrow" />
+          </button>
+          <button onClick={budgetChangeDecrease}>
+            {" "}
+            <img src="./down.png" style={{ width: "7px" }} alt="Down Arrow" />
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
+
 export default Budget;
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,8 +1,12 @@
 
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const ExpenseTotal = () => {
     const { expenses } = useContext(AppContext);
+
+
+
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
@@ -10,6 +14,9 @@ const ExpenseTotal = () => {
         <div className='alert alert-primary'>
             <span>Spent so far: £{totalExpenses}</span>
         </div>
-    );
+    )
+
+
+;
 };
 export default ExpenseTotal;
